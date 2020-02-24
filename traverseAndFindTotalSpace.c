26fd33a1afecdc55d879a263e3f9bcd2a629e86f
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2020-02-24 07:44:54 +0200

    Create traverseAndFindTotalSpace.c

---
+++ traverseAndFindTotalSpace.c
@@ -0,0 +1,53 @@
+#include <dirent.h> 
+#include <stdio.h> 
+#include <string.h>
+#include "findFileSize.c"
+
+
+
+double totalSize = 0;
+
+/* let us make a recursive function to print the content of a given folder */
+
+void calculateSpace(char * path)
+{
+  DIR * d = opendir(path); // open the path
+  if(d==NULL) return; // if was not able return
+  struct dirent * dir; // for the directory entries
+  while ((dir = readdir(d)) != NULL) // if we were able to read somehting from the directory
+    {
+      if(dir-> d_type != DT_DIR) // if the type is not directory just print it with blue
+      {  
+        //printf("%s\n", dir->d_name); // print its name in green
+        char filePath[10000]; // here I am using sprintf which is safer than strcat
+        sprintf(filePath, "%s/%s", path, dir->d_name);
+        double size = findSize(filePath)*1e-9;
+
+        if(size != -1 && size < 1000)
+        {
+            //printf("%lf\n", size);
+            totalSize += size;
+        } 
+        
+      }
+      else
+      if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) // if it is a directory
+      {
+        //printf("%s\n", dir->d_name); // print its name in green
+        char d_path[10000]; // here I am using sprintf which is safer than strcat
+        sprintf(d_path, "%s/%s", path, dir->d_name);
+        calculateSpace(d_path); // recall with the new path
+      }
+    }
+    closedir(d); // finally close the directory
+}
+
+int main(int argc, char **argv)
+{
+
+  printf("\n");
+  calculateSpace("/");
+  printf("%lf\n", totalSize);
+  printf("\n");
+  return(0);
+}
