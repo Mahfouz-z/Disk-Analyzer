fe6e2d902559234fcfdf4ad8022b3db8c33ad67b
Mahmoud363 <mahmoudalsayed@aucegypt.com>
2020-02-26 10:08:31 +0200

    tree needs to be fixed

--- fastTraversal.c
+++ fastTraversal.c
@@ -1,41 +1,54 @@
+ 
 #define _XOPEN_SOURCE 500
-#include "ntfw.c"
+#include <ftw.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include "list.h"
-
-#define LIST(name,size,parent) struct *tree
 
 double totalSize = 0;
 
-static int traverse(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
  
-    
-    
-    if (tflag == FTW_NS)
-    {
-        printf("-------\n");
-        //return 1;
-    }
-    else if (tflag == FTW_F)
-    {
-        long int size = (intmax_t) sb->st_size;
-        
-            printf("%-3s %2d ",
+    /*printf("%-3s %2d ",
             (tflag == FTW_D) ?   "d"   : (tflag == FTW_DNR) ? "dnr" :
             (tflag == FTW_DP) ?  "dp"  : (tflag == FTW_F) ?   "f" :
             (tflag == FTW_NS) ?  "ns"  : (tflag == FTW_SL) ?  "sl" :
             (tflag == FTW_SLN) ? "sln" : "???",
-            ftwbuf->level);
-            printf("%7jd", (long int) sb->st_size);
-            printf("   %-40s %d %s\n", fpath, ftwbuf->base, fpath + ftwbuf->base);
-        
+            ftwbuf->level);*/
+    
+    if (tflag == FTW_NS)
+    {
+        printf("-------");
+    }
+    else if (tflag == FTW_F)
+    {
+        long int size = (intmax_t) sb->st_size;
+        //printf("%7jd", (intmax_t) sb->st_size);
+        //printf("%ld\n", size);
+        //if(size < 7200000)
+        //{
+            totalSize += (float)size * 1e-9;
+        //}
+        //else
+        //{
+            /*printf("%-3s %2d ",
+            (tflag == FTW_D) ?   "d"   : (tflag == FTW_DNR) ? "dnr" :
+            (tflag == FTW_DP) ?  "dp"  : (tflag == FTW_F) ?   "f" :
+            (tflag == FTW_NS) ?  "ns"  : (tflag == FTW_SL) ?  "sl" :
+            (tflag == FTW_SLN) ? "sln" : "???",
+            ftwbuf->level);*/
+            //printf("%7jd", (long int) sb->st_size);
+           // printf("   %-40s %d %s\n", fpath, ftwbuf->base, fpath + ftwbuf->base);
+        //}
         
 
-    }       
+    }
+
+    
+    //printf("   %-40s %d %s\n", fpath, ftwbuf->base, fpath + ftwbuf->base);
+       
 
     return 0;           /* To tell nftw() to continue */
 }
@@ -43,7 +56,7 @@ static int traverse(const char *fpath, const struct stat *sb, int tflag, struct 
 int main(int argc, char *argv[])
 {
     int flags = 0;
-    
+
     if (argc > 2 && strchr(argv[2], 'd') != NULL)
         flags |= FTW_DEPTH;
     if (argc > 2 && strchr(argv[2], 'p') != NULL)
@@ -51,11 +64,11 @@ int main(int argc, char *argv[])
 
     
 
-    if (nftws((argc < 2) ? "/" : argv[1], traverse, 100, flags)
+    if (nftw((argc < 2) ? "/" : argv[1], display_info, 100, flags)
             == -1) {
         perror("nftw");
-        //printf("Total Size = %lf \n", totalSize);
-        //exit(EXIT_FAILURE);
+        printf("Total Size = %lf \n", totalSize);
+        exit(EXIT_FAILURE);
     }
     printf("Total Size = %lf \n", totalSize);
     exit(EXIT_SUCCESS);
--- scanner.c
+++ scanner.c
@@ -8,6 +8,55 @@
 #include "vector.h"
 #include "vector.c"
 
+#define BLOCKSIZE 1024
+
+int roundUp(int num) {
+   int remainder = abs(num) % 4;
+
+   if (remainder == 0)
+      return num;
+   else 
+      return num + 4 - remainder;
+}
+
+
+int not_folder(char * path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return (S_ISREG(path_stat.st_mode));
+}
+
+double folder_size(char * name)
+{
+  double dir_size = 0L;
+  struct dirent * pDirent;
+  DIR * pDir = opendir(name);
+  while ((pDirent = readdir(pDir)))
+  {
+    if (strcmp (pDirent->d_name, "..") != 0 && strcmp (pDirent->d_name, ".") != 0)
+    {
+      char buf[PATH_MAX];
+      strcpy(buf, name);
+      strcat(buf, "/");
+      strcat(buf, pDirent->d_name);
+      if (not_folder(buf))
+      {
+        struct stat st;
+        stat(buf, &st);
+        dir_size += st.st_blocks * S_BLKSIZE*1e-9;
+        //printf("%s %ld\n", buf, (long)st.st_size);
+      }
+      else
+      {
+        dir_size += folder_size(buf);
+      }
+    }
+  }
+  (void) closedir(pDir);
+  return dir_size;
+}
+
 double directorySize(char *directory_name,struct list* prev )
 {
     double directory_size = 0;
@@ -27,22 +76,33 @@ double directorySize(char *directory_name,struct list* prev )
             strcat(strcat(strcpy(buffer, directory_name), "/"), pDirent->d_name);
             struct list *current = (struct list*)malloc(sizeof(struct list));
             vector_init(&current->next);
-            vector_add(&prev->next,current);
+            
             current->name = buffer;
+            //printf("%s\n",current->name);
             current->size=0;
             struct stat file_stat;
+            if(strcmp(current->name,"//dev/lightnvm/control")==0){
+              int jhg = 0;
+            }
 
             if (pDirent->d_type != DT_DIR)
             {
+              if (strcmp(pDirent->d_name, ".") != 0 && strcmp(pDirent->d_name, "..") != 0){
+                vector_add(&prev->next,current);
+                print_tree(prev);
                 stat(buffer, &file_stat);
-                current->size = (file_stat.st_blocks * S_BLKSIZE)/ (1073741274.0);
+                //roundUp((stats->st_size + (BLOCKSIZE - 1)) / BLOCKSIZE)
+                current->size = file_stat.st_size*1e-9;
                 directory_size+=current->size;
+              }
             }
 
             else if (pDirent->d_type == DT_DIR)
             {
                 if (strcmp(pDirent->d_name, ".") != 0 && strcmp(pDirent->d_name, "..") != 0)
                 {
+                    vector_add(&prev->next,current);
+                    print_tree(prev);
                     current->size += directorySize(buffer,current);
                     directory_size+=current->size;
                 }
@@ -55,14 +115,30 @@ double directorySize(char *directory_name,struct list* prev )
     return directory_size;
 }
 
+long tot;
+void print_tree(struct list *ptr){
+  if(ptr!=NULL){
+    tot +=1;
+    printf("%lf    %s     %d\n",ptr->size,ptr->name,vector_total(&ptr->next));
+    for (int i = 0; i < vector_total(&ptr->next); i++)
+    {
+      
+      struct list *cur = vector_get(&ptr->next,i);
+        printf("%lf    %s\n",ptr->size,cur->name );
+    }
+    
+  }
+}
+
 int main(int argc, char *argv[])
 {
     //printf("%ld",S_BLKSIZE);
     struct list *head = (struct list*)malloc(sizeof(struct list));
     vector_init(&head->next);
-    head->name="/usr";
-    
-    printf("%lfGB\n", directorySize("/lib",head));
+    head->name="/";
 
+    printf("%lf GB\n", directorySize("/",head));
+    //print_tree(head);
+    //printf("%lf",tot);
     return 0;
 }
