c557571e54eb01bbeaad2d9965ae737b6c750f8b
hysamah <57324118+hysamah@users.noreply.github.com>
2020-03-28 23:22:39 +0200

    Add files via upload

--- DiskAnalyzer.py
+++ DiskAnalyzer.py
@@ -174,10 +174,10 @@ class MyFileBrowser(simpleUi.Ui_MainWindow, QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     print("Loaing Disk Tree Scrapper, Please Wait...")
-    #os.system("gcc -o scanner scanner.c") 
+    os.system("gcc -o scanner scanner.c") 
     print("Done Loading Tree Scrapper!")
     print("Producing The Disk Tree, Please Wait...")
-    #os.system('./scanner ' + '/' + ' ' + "> tree.txt")
+    os.system('./scanner ' + '/' + ' ' + "> tree.txt")
     print("done producing the tree!")
     app = QtWidgets.QApplication([])
     fb = MyFileBrowser()
