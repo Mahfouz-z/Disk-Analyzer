83534ad1fa5f0644d44eccdea52bc451837fc3ac
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2020-03-28 17:30:32 +0200

    Update and rename DiskAnalyzer.py to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,77 +1,84 @@
 #!/usr/bin/env python3
 
-
+from PyQt5.QtCore import QObject
 from treeClasses import *
 from parser import *
-import subprocess
-import operator
+
 
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import QtCore
 import os
-#import numpy as np
-import random
-import matplotlib.colors as mcolors
-from collections import OrderedDict
+import numpy as np
 
 from ui import simpleUi
 
 
-from matplotlib.backends.qt_compat import QtCore, QtWidgets, is_pyqt5
-if is_pyqt5():
-    from matplotlib.backends.backend_qt5agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
-else:
-    from matplotlib.backends.backend_qt4agg import (
-        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+
+from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 from matplotlib import cm
-
-# generate random integer values
-from random import seed
-from random import randint
-# seed random number generator
-seed(1)
-
+import operator
+import matplotlib.pyplot as plt
 
 
 class MyFileBrowser(simpleUi.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self, maya=False):
         super(MyFileBrowser, self).__init__()
+        self.parseTree()
         self.setupUi(self)
         self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.pushButton.clicked.connect(self.analyze)
         self.treeView.itemClicked.connect(self.treeClicked)
         self.treeView.setHeaderLabels(["Path", "size", "Type"])
+        
 
+    def parseTree(self):
+        self.disk_tree = Tree() 
+        parserInstance = parser(self.disk_tree)
+        parserInstance.generate(self.disk_tree.head)
+        self.treeViewPermnanet = QtWidgets.QTreeWidget()
+        self.disk_tree.tree_print(self.disk_tree.head, self.treeViewPermnanet)
+        print("done parsing the tree!")
 
     def treeClicked(self, it, col):
-        print(it.text(0))
-        #self.disk_tree = Tree() 
+        path = "/" + it.text(0)
+        while(it.parent() != None and it.parent().text(0) != ""):
+            path = "/" + it.parent().text(0) + path
+            it = it.parent()
+        print(path)
         folderHead = Node("","","") 
-        self.disk_tree.findHead(self.disk_tree.head, it.text(0), folderHead)
+        self.disk_tree.findHead(self.disk_tree.head, path, folderHead)
         self.setPieChart(folderHead)
 
+
     def analyze(self):
-        filePath = self.pathEntry.text()
-        if os.path.exists(filePath):  
-            os.system('./scanner ' + filePath + ' ' + "> tree.txt")
-            print("Done producing the tree!")
-            self.disk_tree = Tree() 
-            parserInstance = parser(self.disk_tree)
-            parserInstance.generate(self.disk_tree.head) 
-            print("Done parsing the tree!")
-            self.populate()
-            self.setPieChart(self.disk_tree.head)
+        filePath = str(self.pathEntry.text())
+        if os.path.exists(filePath):
+            currentPath = os.getcwd()
+            if(filePath[len(filePath)-1] == '/' and len(filePath) != 1):
+                filePath = filePath[:-1]
+            if(filePath == "/." or filePath == "/.."):
+                filePath = "/"
+            if(filePath.find("..") != -1):
+                filePath = filePath.replace("..",os.path.normpath(os.getcwd() + os.sep + os.pardir))
+            elif(filePath.find(".") != -1):
+                filePath = filePath.replace(".",currentPath)
+            
+            #print(filePath)
+
+            folderHead = Node("","","") 
+            self.disk_tree.findHead(self.disk_tree.head, filePath, folderHead)
+            #print(folderHead.npath) 
+            self.populate(folderHead)
+            self.setPieChart(folderHead)
+          
         else:
             self.pathEntry.setText("Invalid Path Please Reenter a valid one!")
 
     def sizeTextToInt(self, size):
         sizeArr = size.split(" ")
         floatSize = float(sizeArr[0])
-        sizeArr=sizeArr[::-1] 
         if(sizeArr[0] == "GiB"):
             floatSize = floatSize * 10e9
         elif(sizeArr[0] == "MiB"):
@@ -124,19 +131,45 @@ class MyFileBrowser(simpleUi.Ui_MainWindow, QtWidgets.QMainWindow):
         self._static_ax.legend(labels=['%s, %1.1f%%' % (l, (float(s) / self.total) * 100)for l, s in major], loc="upper right", bbox_to_anchor=(1, 1, 0.2, 0.2))
         
 
-    def populate(self):
+    def populate(self, folderHead):
         self.treeView.clear()
-        self.disk_tree.tree_print(self.disk_tree.head, self.treeView)
+        root = self.treeViewPermnanet.invisibleRootItem()
+        self.popHelper(folderHead.npath, "", root)
+        print("populated")
         self.treeView.setSortingEnabled(True)
 
+    def popHelper(self, path, currentPath, root):
+        if(currentPath == path):
+            self.treeView.addTopLevelItem(root.clone())
+            print("found!")
+            path = -1
+
+        child_count = root.childCount()
+        for i in range(child_count):
+            if(path == -1): return
+            item = root.child(i)
+            if(currentPath != "/"):
+                searchPath =  currentPath + "/" + item.text(0)
+            else:
+                searchPath =  currentPath + item.text(0)
+            if(path.find(searchPath) != -1):
+                self.popHelper(path, searchPath, item)
+            
+                    
 
 
 if __name__ == '__main__':
-    
-    cmd = ["gcc", "-o", "scanner", "scanner.c"]
-    p = subprocess.Popen(cmd)  
-    p.wait()  
+    print("Loaing Tree Parser, Please Wait!")
+    #os.system("gcc -o scanner scanner.c") 
+    print("Done Loading Tree Parser!")
+    print("Producing The Disk Tree, Please Wait...")
+    #os.system('./scanner ' + '/' + ' ' + "> tree.txt")
+    print("done producing the tree!")
     app = QtWidgets.QApplication([])
     fb = MyFileBrowser()
     fb.show()
     app.exec_()
+
+
+
+
